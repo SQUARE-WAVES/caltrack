237b30b0e296d4158fc9532fea48c1c78d508341
SQUARE-WAVES <michael.behrman@gmail.com>
2020-05-24 21:49:46 -0500

    made it so args can change config paths

--- main.js
+++ main.js
@@ -5,17 +5,20 @@ const tag_function = require("./lib/tag_function.js");
 
 const read_conf = util.promisify(fs.readFile)
 
-const main = async () => {
-	const env_config = await read_conf("./conf/test.tj","utf8");
+const main = async (env_config_path,conf_path) => {
+	const env_config = await read_conf(env_config_path,"utf8");
 	const env = await tj(env_config,tag_function());
 
-	const conf = await read_conf("./conf/server.tj","utf8");
+	const conf = await read_conf(conf_path,"utf8");
 	const serv = await tj(conf,tag_function(env));
 
 	serv.listen();
 }
 
-main().then(
+const env_path = process.argv[2] || "./conf/test.tj";
+const serv_path = process.argv[3] || "./conf/server.tj";
+
+main(env_path,serv_path).then(
 	win => console.log("wow, we did it"),
 	fail => {
 		console.log("oh no it all went wrong")
